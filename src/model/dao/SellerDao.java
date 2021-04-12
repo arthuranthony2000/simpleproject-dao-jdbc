968976b0bcc2bea44d692810640a8299987801f4
Arthur Anthony <arthuranthony2000@gmail.com>
2021-04-12 02:16:19 -0300

    Implementando findById do SellerDaoJDBC

--- src/model/dao/SellerDao.java
+++ src/model/dao/SellerDao.java
@@ -8,6 +8,6 @@ public interface SellerDao {
 	public void insert(Seller obj);
 	public void update(Seller obj);
 	public void deleteById(Integer id);
-	public void findById(Integer id);
+	public Seller findById(Integer id);
 	public List<Seller> findAll();
 }
