1069fe8320d6f96222b77c43b545fad26a0f937b
Arthur Anthony <arthuranthony2000@gmail.com>
2021-04-15 01:29:01 -0300

    Inserindo funcionalidade de inserção

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -13,14 +14,22 @@ public class Program {
 		
 		System.out.println("### TEST 1: seller findById ###");
 		System.out.println(sellerDao.findById(1));
+		System.out.println();
 		
 		System.out.println("### TEST 2: sellers findByDepartment ###");
 		Department department = new Department(1, null);
 		List<Seller> list = sellerDao.findByDeparment(department);
 		list.forEach(System.out::println);
+		System.out.println();
 		
 		System.out.println("### TEST 3: sellers findAll ###");
 		list = sellerDao.findAll();
 		list.forEach(System.out::println);
+		
+		System.out.println("### TEST 4: seller insert ###");
+		Seller obj = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.00d, department);
+		sellerDao.insert(obj);
+		System.out.println("Inserted Seller Successful, Id Seller --> "+obj.getId());
 	}
 }
+
